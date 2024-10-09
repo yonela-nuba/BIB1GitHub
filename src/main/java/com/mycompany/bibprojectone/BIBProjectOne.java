051a7d04642a36e91f3f4da6d00195175e9ae81a
ynuba <ynuba@RCGPBRACA03.vpn.advtech.co.za>
2024-10-09 08:40:43 +0200

    Created an application that adds two numbers and display the sum

--- src/main/java/com/mycompany/bibprojectone/BIBProjectOne.java
+++ src/main/java/com/mycompany/bibprojectone/BIBProjectOne.java
@@ -11,6 +11,11 @@ package com.mycompany.bibprojectone;
 public class BIBProjectOne {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        int a = 5;
+        
+        int b = 10;
+        int c = a+b;
+        System.out.println(c);
     }
 }
