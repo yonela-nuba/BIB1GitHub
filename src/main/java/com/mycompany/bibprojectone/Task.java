3526534e47db03aded801c69972980a10b816150
ynuba <ynuba@RCGPBRACA03.vpn.advtech.co.za>
2024-10-09 08:50:10 +0200

    Added an external class Called Task

---
+++ src/main/java/com/mycompany/bibprojectone/Task.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bibprojectone;
+
+/**
+ *
+ * @author ynuba
+ */
+public class Task {
+    int a = 10;
+    
+}
