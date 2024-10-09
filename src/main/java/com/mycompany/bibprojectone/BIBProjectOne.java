aec5e8090021c41e39aefc07ca79b45c3e1b3656
ynuba <ynuba@RCGPBRACA03.vpn.advtech.co.za>
2024-10-09 08:26:17 +0200

    First Commit

---
+++ src/main/java/com/mycompany/bibprojectone/BIBProjectOne.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bibprojectone;
+
+/**
+ *
+ * @author ynuba
+ */
+public class BIBProjectOne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
